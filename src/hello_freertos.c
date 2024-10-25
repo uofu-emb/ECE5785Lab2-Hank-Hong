f687e8e7491471e617f7dcd29a54bd76fff0bcdc
Hank <henryhanksthompson@gmail.com>
2024-10-25 11:19:26 -0600

    committing a testing scenario

--- src/hello_freertos.c
+++ src/hello_freertos.c
@@ -21,26 +21,24 @@ bool on = false;
 #define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 #define BLINK_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
+// blinks the LED on and off using a 500ms delay
 void blink_task(__unused void *params) {
     hard_assert(cyw43_arch_init() == PICO_OK);
     while (true) {
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
-        if (count++ % 11) on = !on;
+        on = do_blink(on, &count);
         vTaskDelay(500);
     }
 }
 
+// creates a task to blink the LED
 void main_task(__unused void *params) {
     xTaskCreate(blink_task, "BlinkThread",
                 BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
     char c;
-    while(c = getchar()) {
-        if (c <= 'z' && c >= 'a') putchar(c - 32);
-        else if (c >= 'A' && c <= 'Z') putchar(c + 32);
-        else putchar(c);
-    }
+    while(c = getchar()) putchar(switch_case(c));
 }
 
+// initializes and schedules the tasks
 int main( void )
 {
     stdio_init_all();
---
+++ test/manual/lab2.md
@@ -0,0 +1,15 @@
+#How to set up test scenario:
+
+-Build and flash the project onto the pico board
+-Don't forget to reboot
+
+#How to exercise the system:
+
+-Open a serial monitor
+-Type a letter
+
+#Expected behavior:
+
+-You should be able to observe an upper case letter become lower case and vice versa
+-LED will blink
+-If you don't type a letter, it will return what you typed.
